c033000c67d6ecd599dd11d3a3563de1fc409925
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2025-01-15 13:29:50 +0100

    calendario y foto de perfil añadida

--- src/app/main/components/home/home.component.ts
+++ src/app/main/components/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid'; // Asegúrate de tener instalado este plugin
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { CharlasService } from '../../../services/charlas-service.service';
+import { Ronda } from '../../../models/charla';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,10 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // Asegúrate de tener instal
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css', '../../../app.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public rondas!: Ronda[];
+
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin], // Plugin necesario para la vista dayGridMonth
     initialView: 'dayGridMonth',
@@ -17,9 +22,33 @@ export class HomeComponent {
       center: 'title', // Título centrado
       right: 'prev,next', // Botones prev y next,
     },
-    events: [
-      { title: 'Evento 1', date: '2025-01-07', color: '#2b2e38', textColor: '#ffeba7' },
-      { title: 'Evento 2', date: '2025-01-10', color: '#2b2e38', textColor: '#ffeba7' },
-    ], // Ejemplo de eventos
+    events: [] // Inicialmente vacío
   };
+
+  constructor(private charlasService: CharlasService) {}
+
+  ngOnInit(): void {
+    this.charlasService.getRondasCurso().subscribe((response: Ronda[]) => {
+      const events = response.map((ronda: Ronda) => {
+        const localDate = new Date(ronda.fechaPresentacion);
+        const formattedDate = new Date(localDate.getTime() - localDate.getTimezoneOffset() * 60000)
+          .toISOString()
+          .split('T')[0]; // Ajusta la fecha y elimina la parte de tiempo
+        return {
+          title: ronda.descripcionModulo,
+          date: formattedDate, // Usa la fecha ajustada
+          color: '#2b2e38',
+          textColor: '#ffeba7',
+        };
+      });
+
+      // Asigna los eventos al calendario
+      this.calendarOptions = {
+        ...this.calendarOptions, // Mantén las opciones actuales
+        events, // Actualiza los eventos
+      };
+      console.log(this.calendarOptions.events); // Verifica los eventos en consola
+    });
+  }
+
 }
