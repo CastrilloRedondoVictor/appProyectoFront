17911e5455da98944fd4a50a9e712a73298b24d2
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-16 23:45:11 +0100

    crear comentario con otro estilo

--- src/app/charlas/components/detalles-charla/detalles-charla.component.ts
+++ src/app/charlas/components/detalles-charla/detalles-charla.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Charla } from '../../../models/charla';
 import { ActivatedRoute } from '@angular/router';
 import { CharlasService } from '../../../services/charlas-service.service';
-import { CharlaDetalles } from '../../../models/charlaDetalles';
+import { CharlaDetalles, ComentariosSin } from '../../../models/charlaDetalles';
+import { Perfil } from '../../../models/alumno';
+import { AuthService } from '../../../services/auth-service.service';
 
 @Component({
   selector: 'app-detalles-charla',
@@ -16,15 +18,23 @@ export class DetallesCharlaComponent implements OnInit {
   charlasRonda!: Charla[];
   charlaDetalles!: CharlaDetalles;
   idRonda!: number;
+  public comentario!: ComentariosSin;
+  public perfil!: Perfil;
   comentariosAbiertos: boolean = false;
   recursosAbiertos: boolean = false;
+  nuevoComentarioAbierto: boolean = false; // Controla la visibilidad del formulario
+  nuevoComentarioContenido: string = ''; // Contenido del nuevo comentario
 
   constructor(
     private route: ActivatedRoute,
-    private charlasService: CharlasService
+    private charlasService: CharlasService,
+    private _servicePerfil: AuthService
   ) {}
 
   ngOnInit(): void {
+    this._servicePerfil.getPerfil().subscribe((response) => {
+      this.perfil = response.usuario;
+    });
     // Obtener ID de ronda y charla desde los parámetros de la ruta
     this.idRonda = Number(this.route.snapshot.paramMap.get('idRonda'));
     this.idCharla = Number(this.route.snapshot.paramMap.get('idCharla'));
@@ -60,7 +70,27 @@ export class DetallesCharlaComponent implements OnInit {
     this.comentariosAbiertos = !this.comentariosAbiertos;
   }
 
+  toggleNuevoComentario(): void {
+    this.nuevoComentarioAbierto = !this.nuevoComentarioAbierto;
+  }
+
   toggleRecursos(): void {
     this.recursosAbiertos = !this.recursosAbiertos;
+  }
+
+  crearComentario(): void {
+    this.comentario = new ComentariosSin(
+      1,
+      this.idCharla,
+      this.perfil.idUsuario,
+      this.nuevoComentarioContenido,
+      new Date().toISOString()
+    );
+
+    this.charlasService
+      .postComentario(this.comentario)
+      .subscribe((response: ComentariosSin) => {
+        alert('Comentario creado');
+      });
   }
 }
