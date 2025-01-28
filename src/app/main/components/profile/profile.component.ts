3078119d108a3a3b3ba0b98f79afdb52d01c84ac
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-28 14:19:05 +0100

    Charlas en el perfil del alumno

--- src/app/main/components/profile/profile.component.ts
+++ src/app/main/components/profile/profile.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Perfil } from '../../../models/alumno';
 import { AuthService } from '../../../services/auth-service.service';
 import { AlumnosCursoProfesor } from '../../../models/perfil';
+import { Charla } from '../../../models/charla';
+import { CharlasService } from '../../../services/charlas-service.service';
+import { CharlaDetalles } from '../../../models/charlaDetalles';
 
 @Component({
   selector: 'app-profile',
   standalone: false,
 
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css', '../../../app.component.css']
+  styleUrls: ['./profile.component.css', '../../../app.component.css'],
 })
 export class ProfileComponent implements OnInit {
   public perfil!: Perfil;
@@ -16,18 +19,33 @@ export class ProfileComponent implements OnInit {
   public alumnosCursos!: AlumnosCursoProfesor[];
   public listaCursos: any[] = [];
   public cursosAbiertos: { [key: number]: boolean } = {};
+  public charlasAlumno!: CharlaDetalles[];
 
-  constructor(private _service: AuthService) {}
+  constructor(
+    private _service: AuthService,
+    private charlasService: CharlasService
+  ) {}
 
   ngOnInit(): void {
     this._service.getPerfil().subscribe((response) => {
       this.perfil = response.usuario;
     });
-    this.rolUsuario = this._service.getRolUsuario()
-    if(this.rolUsuario == '1') {
-      this._service.getAlumnosCursoProfesor().subscribe((response: AlumnosCursoProfesor[]) => {
-        this.alumnosCursos= response
-      });
+    this.rolUsuario = this._service.getRolUsuario();
+    if (this.rolUsuario == '2') {
+      this.charlasService
+        .getCharlasAlumno()
+        .subscribe((response: CharlaDetalles[]) => {
+          this.charlasAlumno = response;
+          console.log(this.charlasAlumno);
+          console.log(this.rolUsuario);
+        });
+    }
+    if (this.rolUsuario == '1') {
+      this._service
+        .getAlumnosCursoProfesor()
+        .subscribe((response: AlumnosCursoProfesor[]) => {
+          this.alumnosCursos = response;
+        });
       // this.alumnosCursos.filter(alumno => alumno.alumnos.alumno.idRole == 2)
     }
 
@@ -38,6 +56,10 @@ export class ProfileComponent implements OnInit {
     }
   }
 
+  getHoras(tiempo: number): number {
+    return Math.floor(tiempo / 60);
+  }
+
   // Método para abrir/cerrar los detalles de cada curso
   toggleCurso(idCurso: number): void {
     this.cursosAbiertos[idCurso] = !this.cursosAbiertos[idCurso];
@@ -45,16 +67,20 @@ export class ProfileComponent implements OnInit {
 
   // Método para cambiar estado entre true/false
   toggleActivo(curso: any): void {
-    const nuevoEstado = !curso.activo; 
-  
+    const nuevoEstado = !curso.activo;
+
     this._service.updateEstadoCurso(curso.idCurso, nuevoEstado).subscribe({
       next: () => {
-        curso.activo = nuevoEstado; 
-        console.log(`Curso ${curso.nombre} actualizado correctamente a ${nuevoEstado ? 'Activo' : 'Inactivo'}.`);
+        curso.activo = nuevoEstado;
+        console.log(
+          `Curso ${curso.nombre} actualizado correctamente a ${
+            nuevoEstado ? 'Activo' : 'Inactivo'
+          }.`
+        );
       },
       error: (err) => {
         console.error('Error al actualizar el curso:', err);
-      }
+      },
     });
   }
 
